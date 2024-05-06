901dce7b07c037d9417d8bdf9d7b8da5aa9ee8fa
Andrey.T <taranichenko.a@gmail.com>
2024-05-06 11:12:28 +0300

    fix program.cs

--- test.cs
+++ test.cs
@@ -1,5 +1,5 @@
-class Program{
-    public static void Main{
-        Console.WriteLine("Hello Git"),
+class Program {
+    public static void Main {
+        Console.WriteLine("Hello Git");
     }
     }
